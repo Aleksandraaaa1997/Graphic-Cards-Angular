73684065a8d6a882ae770d10e37b9084bcbe8700
Aleksandra Lijeskic <analijeskic97@gmail.com>
2022-05-11 15:06:06 +0200

    Project Finished

---
+++ GraphicCards/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { HomeComponent } from './core/home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardComponent } from './cards/card/card.component';
+import { CommentsComponent } from './comments/comments.component';
+import { CommentComponent } from './comment/comment.component';
+import { PaginationComponent } from './cards/pagination/pagination.component';
+import { FormsModule } from '@angular/forms';
+import { ShowMoreComponent } from './cards/card/show-more/show-more.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    CardsComponent,
+    CardComponent,
+    CommentsComponent,
+    CommentComponent,
+    PaginationComponent,
+    ShowMoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GraphicCards/src/app/cards/card/show-more/show-more.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-more',
+  templateUrl: './show-more.component.html',
+  styleUrls: ['./show-more.component.css']
+})
+export class ShowMoreComponent implements OnInit {
+
+  @Input()
+  text: string = "";
+
+  @Input()
+  textLength: number = 130;
+
+  isShowMore: boolean = false;
+  showText: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.toggleShow()
+  }
+
+  toggleShow() {
+    this.isShowMore = !this.isShowMore;
+    if(!this.isShowMore || this.textLength > this.text.length) {
+      this.showText = this.text;
+    } else {
+      this.showText = this.text.substr(0, this.textLength) + "..."
+    }
+  }
+}
